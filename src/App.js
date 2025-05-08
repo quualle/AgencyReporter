4203598676ed32ec96c8000dcc6b6646a3b9b967
quualle <marcoheer@live.de>
2025-05-08 02:17:53 +0200

    Problematic stays seite 50%

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: 40}}>
+      <h1>AgencyReporter</h1>
+      <p>Deine React-App läuft!</p>
+    </div>
+  );
+}
+
+export default App; 
